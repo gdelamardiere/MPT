da3ae47adeb9ea340b68b48b74ce261df9c41a40
gdelamardiere <gdelamardiere@gmail.com>
2013-02-21 18:25:40 +0100

    temporaire

--- classes/spreedsheet.php
+++ classes/spreedsheet.php
@@ -48,21 +48,19 @@ class spreedsheet {
 
 
 
+
 	public function __construct(){
 		$service = Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME;
 		$client = Zend_Gdata_ClientLogin::getHttpClient(USERNAME, PASSWORD, $service);
 		$this->spreadSheetService = new Zend_Gdata_Spreadsheets($client);
-		/*$query = new Zend_Gdata_Spreadsheets_DocumentQuery();
-		$query->setSpreadsheetKey(KEY);
-		$feed = $this->spreadSheetService->getWorksheetFeed($query);
-		$entries = $feed->entries[0]->getContentsAsRows();
-		$this->columnCount=(isset($entries[0]))?sizeof($entries[0]):0;//verifier si nécéssaire*/
-		$this->columnCount=4;
 
-		//$this->getColumn();
+		$this->columns=unserialize(file_get_contents("entete_fichier_google.txt"));
+		$this->columnCount=sizeof($this->columns);
 	}
 
 	public function add_personne($aData=null){
+		$this->getColumn();
+		return false;
 		/*var_dump($this->columns);
 		$this->getColumn();
 		var_dump($this->columns);*/
@@ -86,6 +84,11 @@ class spreedsheet {
 
 	public function getColumn(){
 		$this->columns=array();
+		$query = new Zend_Gdata_Spreadsheets_DocumentQuery();
+		$query->setSpreadsheetKey(KEY);
+		$feed = $this->spreadSheetService->getWorksheetFeed($query);
+		$entries = $feed->entries[0]->getContentsAsRows();
+		$this->columnCount=(isset($entries[0]))?sizeof($entries[0]):0;
 		$query = new Zend_Gdata_Spreadsheets_CellQuery();
 		$query->setSpreadsheetKey(KEY);
 		$feed = $this->spreadSheetService->getCellFeed($query);
@@ -95,11 +98,51 @@ class spreedsheet {
 				$this->columns[$i] = $this->filtreName($columnName);
 			}    
 		}
+		file_put_contents("entete_fichier_google.txt",serialize($this->columns));
 	}
 
 
 	public function filtreName($columnName){
 		return str_replace("'","",strtolower(str_replace(' ', '', $columnName)));//rajouter :
+	}
+
+
+
+	public function getRelationDbGd($actif=""){
+		$where=($actif==1||$actif==0)?"WHERE actif='".$actif."'":"";
+		$stmt = $this->pdo->prepare("SELECT * FROM `r_db_gd`  ".$where);
+		$stmt->execute() ;
+		$ret=array();
+		while($relation=$stmt->fetch(PDO::FETCH_ASSOC)){
+			$ret[]=$relation;
+		}
+		return $ret;
+	}
+
+	public function InsertRelationDbGd($aData){
+		if(empty($aData["name_db"]) || empty($aData["table_db"])){
+			$aData["name_db"]=null;
+			$aData["table_db"]=null;
+			$aData["actif"]='0';
+		}
+		$stmt = $this->pdo->prepare("INSERT INTO  `lmpt`.`r_db_gd` (
+			`id_r_db_gd` ,`name_gd` ,`position_gd` ,`name_db` ,`table_db` ,`actif`
+			)
+		VALUES (
+			NULL ,  :name_gd,  :position_gd,  :name_db, 
+			:table_db,  :actif)"
+		);
+		$stmt->execute($aValue);
+	}
+
+
+	public function majRelationDbGd(){
+		$aRelation=$this->getRelationDbGd();
+		$aColumns=$this->getColumn();
+		//insertion nouveau champ en base
+		foreach($aColumns as $key=>$value){
+			if()
+		}
 	}
 
 
--- conf.php
+++ conf.php
@@ -9,8 +9,8 @@ define("PASSWORD" , 'mzlluhgycwrzcare'); // Your google account password
 //-------------------------------------------------------------------------------
 // Document key - get it from browser addres bar query key for your open spreadsheet
 
-//define("KEY" , '0As2mRV6eabWQdFJ3TUlDRDFrNXZna0toLUY1dXYySUE');
-define("KEY" , '0AmGreq0d2wLTdDQ5UmE4UUJkSW1CRW5OVHlTY3Z2V2c');
+//define("KEY" , '0AmGreq0d2wLTdDQ5UmE4UUJkSW1CRW5OVHlTY3Z2V2c');
+define("KEY" , '0Ah2z2NTLfq2bdDJQMmtQTVZXNnBSTXJYR3FfcGg1bFE');
 
 //-------------------------------------------------------------------------------
 // base

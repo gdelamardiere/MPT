496b835c0d5dcd6b5737ff9c003d317ce7ae72dc
gdelamardiere <gdelamardiere@gmail.com>
2013-03-08 11:20:49 +0100

    suppression fichiers inutile en locale

--- conf.php
+++ conf.php
@@ -5,12 +5,14 @@
 define("USERNAME" , 'gdelamardiere@gmail.com'); // Your google account username
 define("PASSWORD" , 'mzlluhgycwrzcare'); // Your google account password
 define("EMAIL_MANIF", "recrutement@lamanifpourtous.fr");
+define("EMAIL_FROM", "noreply@recrutementlamanifpourtous.fr");
 define("EMAIL_ADMIN", "gdelamardiere@gmail.com");
 //-------------------------------------------------------------------------------
 // Document key - get it from browser addres bar query key for your open spreadsheet
 
 //define("KEY" , '0AmGreq0d2wLTdDQ5UmE4UUJkSW1CRW5OVHlTY3Z2V2c');
-define("KEY" , '0Ah2z2NTLfq2bdDJQMmtQTVZXNnBSTXJYR3FfcGg1bFE');
+//define("KEY" , '0Ah2z2NTLfq2bdDJQMmtQTVZXNnBSTXJYR3FfcGg1bFE');
+define("KEY" , '0Ah2z2NTLfq2bdDVHaFhSRGhQY3JNSXN4NkpkOGxGOVE');
 
 //-------------------------------------------------------------------------------
 // base
